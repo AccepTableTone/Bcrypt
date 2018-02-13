554c7fdb2516ac93bf50b081dffdcfa5bae7d48e
David McKinnon <dmckinnon@guestlogix.com>
2018-02-13 15:06:12 -0500

    initial commit
    simple bcrypt proof

---
+++ BCrypt/Controllers/BCryptController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BCrypt.Controllers
+{
+    [EnableCors("CorsPolicy")]
+    public class BCryptController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/knock")]
+        public string Knock()
+        {
+            return "i got it";
+        }
+
+        [HttpGet]
+        [Route("api/hashpassword/{pwd}")]
+        public ClientResponse HashPassword(string pwd)
+        {
+            return new ClientResponse()
+            {
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(pwd),
+                Password = pwd
+            };
+        }
+
+        [HttpGet]
+        [Route("api/validatehash/{pwd}/{hash}")]
+        public ClientResponse validateHash(string pwd, string hash)
+        {
+            return new ClientResponse()
+            {
+                PasswordHash = hash,
+                Password = pwd,
+                IsValid = BCrypt.Net.BCrypt.Verify(pwd, hash)
+            };
+        }
+
+
+    }
+
+    public class ClientResponse
+    {
+        public string PasswordHash { get; set; }
+        public string Password { get; set; }
+        public bool IsValid { get; set; }
+
+    }
+}
